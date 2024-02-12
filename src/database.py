c5523ef09b4999cb726f1e29ac9bb037a3714fc1
Sam Andrello <samqandrello@gmail.com>
2024-02-12 13:04:28 -0500

    Added function to upload files

--- src/database.py
+++ src/database.py
@@ -2,16 +2,18 @@
 ### SOURCE: https://thepythoncode.com/article/using-google-drive--api-in-python ###
 
 
-import os
+from __future__ import print_function
+
+import os.path
 import pickle
 
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from tabulate import tabulate
+from googleapiclient.http import MediaFileUpload
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly", "https://www.googleapis.com/auth/drive.file"]
 
 
 def get_gdrive_service():
@@ -85,3 +87,25 @@ def list_files(items):
         table = tabulate(rows, headers=["ID", "Name", "Parents", "Size", "Type", "Modified Time"])
         # print the table
         print(table)
+
+
+def upload_files():
+    """
+    Creates a folder and upload a file to it
+    """
+    # authenticate account
+    service = get_gdrive_service()
+    # folder details we want to make
+    folder_metadata = {"name": "TestFolder", "mimeType": "application/vnd.google-apps.folder"}
+    # create the folder
+    file = service.files().create(body=folder_metadata, fields="id").execute()
+    # get the folder id
+    folder_id = file.get("id")
+    print("Folder ID:", folder_id)
+    # upload a file text file
+    # first, define file metadata, such as the name and the parent folder ID
+    file_metadata = {"name": "test.txt", "parents": [folder_id]}
+    # upload
+    media = MediaFileUpload("test.txt", resumable=True)
+    file = service.files().create(body=file_metadata, media_body=media, fields="id").execute()
+    print("File created, id:", file.get("id"))

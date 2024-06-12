123334cfe807ce34bf9691ca8e593b2bb5870732
Sam Andrello <samqandrello@gmail.com>
2024-06-12 13:16:06 -0400

    Refactored generator.py to work as a class, added relevant parts to ActionGraph class

--- src/action_graph.py
+++ src/action_graph.py
@@ -1,3 +1,4 @@
+from generator import Generator
 import matplotlib.pyplot as plt
 import networkx as nx
 from node_data import NodeData
@@ -5,12 +6,14 @@ import numpy as np
 import random
 
 class ActionGraph:
-    def __init__(self, max_nodes : int, param_types : dict) -> None:
+    def __init__(self, max_nodes : int, param_types : dict, generator : Generator) -> None:
         self.adj_matrix = np.zeros((max_nodes, max_nodes))
         self.data = NodeData(max_nodes, param_types)
         self.max_nodes = max_nodes
-        self.graph = self._to_graph()
         self.param_types = param_types
+        self.generator = generator
+        generator.fill(self)
+        self.graph = self._to_graph()
 
     def _to_graph(self) -> nx.DiGraph:
         # Creates a networkx graph object from the adjacency matrix

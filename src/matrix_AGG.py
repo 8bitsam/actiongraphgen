b48a12b33a1de8a46d989aef26b48ee785a477b4
Sam Andrello <samqandrello@gmail.com>
2024-06-12 14:27:58 -0400

    Complete init for matrix action graph generator class MatrixAGG, which extends Generator

---
+++ src/matrix_AGG.py
@@ -0,0 +1,28 @@
+from action_graph import ActionGraph
+from generator import Generator
+import matplotlib.pyplot as plt
+import networkx as nx
+from node_data import NodeData
+import numpy as np
+import random
+
+
+class MatrixAGG(Generator):
+    def __init__(self, max_nodes : int) -> None:
+        param_types = {
+            "matrix" : np.ndarray,
+            "op" : int,
+            "scalar" : int
+        }
+        self.param_types = param_types
+        operations = {
+            0 : "None",
+            1 : "Inverse",
+            2 : "Addition",
+            3 : "Subtraction",
+            4 : "Multiplication"
+        }
+        self.operations = operations
+        super().__init__(self, max_nodes, param_types, processes=operations)
+    
+    

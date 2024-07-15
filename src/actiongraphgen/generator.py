54fbc32fcb053746be043c2826a22a2e98405ed8
Sam Andrello <samqandrello@gmail.com>
2024-07-15 16:05:19 -0400

    Cookiecutter refactor p2

---
+++ src/actiongraphgen/generator.py
@@ -0,0 +1,9 @@
+from action_graph import ActionGraph
+
+
+class Generator:
+    def __init__(self, max_nodes: int, param_types: dict, processes: dict) -> None:
+        self.max_nodes = max_nodes
+        self.param_types = param_types
+        self.processes = processes
+        self.action_graph = ActionGraph(max_nodes, param_types)

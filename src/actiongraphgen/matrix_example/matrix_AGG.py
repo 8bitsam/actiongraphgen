72c8423103c3328e04955bb509846c7ddedbeff4
Sam Andrello <samqandrello@gmail.com>
2024-07-16 14:36:35 -0400

    run black - reformatting

--- src/actiongraphgen/matrix_example/matrix_AGG.py
+++ src/actiongraphgen/matrix_example/matrix_AGG.py
@@ -5,19 +5,9 @@ import random
 
 class MatrixAGG(Generator):
     def __init__(self, max_nodes: int, matrix: np.ndarray) -> None:
-        param_types = {
-            "matrix": np.ndarray,
-            "op": int,
-            "scalar": int
-        }
+        param_types = {"matrix": np.ndarray, "op": int, "scalar": int}
         self.param_types = param_types
-        operations = {
-            0: "None",
-            1: "Inverse",
-            2: "Addition",
-            3: "Subtraction",
-            4: "Multiplication"
-        }
+        operations = {0: "None", 1: "Inverse", 2: "Addition", 3: "Subtraction", 4: "Multiplication"}
         self.operations = operations
         super().__init__(max_nodes, param_types, operations)
         self.matrix = matrix  # terminal matrix as input to inverse problem

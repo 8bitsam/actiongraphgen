bee96a2145b18d379d75422aafaf261b9afb92f1
Sam Andrello <samqandrello@gmail.com>
2024-07-30 12:06:20 -0400

    document matrix_AGG.py

--- src/actiongraphgen/matrix_example/matrix_AGG.py
+++ src/actiongraphgen/matrix_example/matrix_AGG.py
@@ -1,3 +1,17 @@
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/8bitsam/actiongraphgen/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
 import random
 
 import numpy as np
@@ -6,7 +20,19 @@ from src.actiongraphgen.structure.generator import Generator
 
 
 class MatrixAGG(Generator):
+    """An example action graph generator (AGG) class that uses matrix operations. It extends the
+    :class `actiongraphgen.structure.generator` class.
+
+    :param max_nodes: The maximum number of nodes in the graph.
+    :type max_nodes: int
+
+    :param: matrix: The input data for this generator. It should be a square matrix containing integers.
+    :type matrix: np.ndarray
+    """
+
     def __init__(self, max_nodes: int, matrix: np.ndarray) -> None:
+        """Constructor method
+        """
         param_types = {"matrix": np.ndarray, "op": int, "scalar": int}
         self.param_types = param_types
         operations = {0: "None", 1: "Inverse", 2: "Addition", 3: "Subtraction", 4: "Multiplication"}
@@ -15,6 +41,8 @@ class MatrixAGG(Generator):
         self.matrix = matrix  # terminal matrix as input to inverse problem
 
     def datagen(self) -> None:
+        """Randomly fill in the node data for the purposes of creating random training data.
+        """
         for i in range(self.max_nodes):
             # get random values and assign them to list
             scalar = random.randint(1, 100)

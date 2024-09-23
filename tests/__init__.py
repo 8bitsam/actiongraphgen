715b5f699a7abc2ff537986d2961343b8150b2dd
Sam Andrello <samqandrello@gmail.com>
2024-09-23 14:33:56 -0400

    update init and pyproject.toml for tests

---
+++ tests/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/8bitsam/actiongraphgen/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+"""actiongraphgen tests - Run with python -m tests.run"""
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+# End of file

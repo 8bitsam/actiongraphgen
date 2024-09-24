3f91c76a90618c64a2537315813945232d61bff3
Sam Andrello <samqandrello@gmail.com>
2024-09-24 16:11:47 -0400

    add testing for ActionGraph stream input

---
+++ tests/math_action_graph.py
@@ -0,0 +1,45 @@
+#############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/8bitsam/actiongraphgen/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+#############################################################################
+
+from actiongraphgen.structure.node_data import NodeData
+
+
+def _process_add(input_data: float, node_params: dict) -> float:
+    """Add a specified amount to the input data."""
+    amount = node_params.get('amount', 0)
+    return input_data + amount
+
+
+def _process_multiply(input_data: float, node_params: dict) -> float:
+    """Multiply the input data by a specified factor."""
+    factor = node_params.get('factor', 1)
+    return input_data * factor
+
+
+class MathNodeData(NodeData):
+    """A subclass of NodeData for performing basic math operations on nodes."""
+
+    def process_node(self, node_pos: int, input_data: float) -> float:
+        """Process input data by performing basic math operations.
+        Node parameters must include 'operation' (e.g., 'add', 'multiply') and relevant values.
+        """
+        node_params = self.data_list[node_pos]
+        operation = node_params.get('operation')
+
+        if operation == 'add':
+            return _process_add(input_data, node_params)
+        elif operation == 'multiply':
+            return _process_multiply(input_data, node_params)
+        else:
+            return input_data
---
+++ tests/test_math_action_graph.py
@@ -0,0 +1,48 @@
+#############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/8bitsam/actiongraphgen/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+#############################################################################
+
+import pytest
+from actiongraphgen.structure.action_graph import ActionGraph
+from .math_action_graph import MathNodeData
+
+
+@pytest.fixture
+def math_graph():
+    """Create a math action graph for testing."""
+    param_types = {"operation": str, "amount": float, "factor": float}
+    graph = ActionGraph(3, param_types, node_data_class=MathNodeData)
+
+    # Manually set up a simple DAG: node 0 -> node 1 -> node 2
+    graph.fill_parents(1, [0])  # node 1 depends on node 0
+    graph.fill_parents(2, [1])  # node 2 depends on node 1
+
+    # Set operations for each node
+    graph.data.data_list[0]['operation'] = 'add'
+    graph.data.data_list[0]['amount'] = 10
+
+    graph.data.data_list[1]['operation'] = 'multiply'
+    graph.data.data_list[1]['factor'] = 2
+
+    return graph
+
+
+@pytest.mark.asyncio
+async def test_math_graph_processing(math_graph):
+    """Test processing a data stream through the math action graph."""
+    input_data = 5
+
+    # Expected processing: (5 + 10) * 2 = 30
+    result = math_graph.process_stream(input_data)
+
+    assert result == 30, f"Expected 30 but got {result}"
